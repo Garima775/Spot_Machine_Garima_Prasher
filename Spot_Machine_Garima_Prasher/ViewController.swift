d7d215a59869d96a1f6c21827d85d050bf128aec
Garima <garimaprashar775@gmail.com>
2020-01-15 12:34:40 -0500

    Initial Commit

---
+++ Spot_Machine_Garima_Prasher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spot_Machine_Garima_Prasher
+//
+//  Created by Garima Prasher on 2020-01-15.
+//  Copyright © 2020 Garima Prasher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
